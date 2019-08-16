8d0193736986418222b4a9e8685392b490dd135f
chuasweechin <sampiochua@gmail.com>
2019-08-16 13:46:00 +0800

    implement unit test for blog backend

---
+++ utils/list_helper.js
@@ -0,0 +1,93 @@
+const totalLikes = (blogs) => {
+    if (blogs.length === 0) {
+        return 0
+    }
+
+    const total = blogs.reduce((a, b) =>  {
+        return { likes: a.likes + b.likes }
+    })
+
+    return total.likes
+}
+
+const favoriteBlog = (blogs) => {
+    blogs = blogs.map((blog) => {
+        delete blog._id
+        delete blog.__v
+        delete blog.url
+
+        return { ...blog }
+    })
+
+    let topBlog = {}
+    let topLikes = 0
+
+    for (let i = 0; i < blogs.length; i++) {
+        if (blogs[i].likes > topLikes) {
+            topLikes = blogs[i].likes
+            topBlog = blogs[i]
+        }
+    }
+
+    return topBlog
+}
+
+const authorWithMostBlogs = (authors) => {
+    authors = authors.map((author) => {
+        delete author._id
+        delete author.__v
+
+        return { ...author }
+    })
+
+    let blogs = 0
+    let topAuthor = {}
+
+    for (let i = 0; i < authors.length; i++) {
+        if (authors[i].blogs > blogs) {
+            blogs = authors[i].blogs
+            topAuthor = authors[i]
+        }
+    }
+
+    return topAuthor
+}
+
+const authorWithMostLikes = (blogs) => {
+    if (blogs.length === 0) {
+        return {}
+    }
+
+    blogs = blogs.map((blog) => {
+        delete blog._id
+        delete blog.__v
+        delete blog.url
+
+        return { ...blog }
+    })
+
+    let likesCount = {}
+    let bestAuthor = { author: '', likes: 0}
+
+    for (let i = 0; i < blogs.length; i++) {
+        if (likesCount[blogs[i].author] === undefined) {
+            likesCount[blogs[i].author] = blogs[i].likes
+        } else {
+            likesCount[blogs[i].author] += blogs[i].likes
+        }
+
+        if (likesCount[blogs[i].author] > bestAuthor.likes) {
+            bestAuthor.author = blogs[i].author
+            bestAuthor.likes = likesCount[blogs[i].author]
+        }
+    }
+
+    return bestAuthor
+}
+
+module.exports = {
+    totalLikes,
+    favoriteBlog,
+    authorWithMostBlogs,
+    authorWithMostLikes
+}
